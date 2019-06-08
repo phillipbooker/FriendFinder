67521fe87f3627f28074ea9f4b60f007dc8078a6
Phillip Booker <phillippbooker@gmail.com>
2019-06-08 12:56:52 -0700

    Adds api routes and dummy friend data.

---
+++ app/data/friends.js
@@ -0,0 +1,8 @@
+var friendArray = [
+    {
+        name: "Jerry",
+        id: "jerry-id"
+    }
+];
+
+module.exports = friendArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,18 @@
+var friendData = require("../data/friends");
+
+//Routing
+module.exports = function(app){
+    app.get("/api/friends", function(req, res){
+        res.json(friendData);
+    });
+
+    app.post("/api/friends", function(req, res){
+        friendData.push(req.body);
+        res.json(true);
+    });
+
+    app.post("/api/clear", function(req, res){
+        friendData.length = 0;
+        res.json({ok: true});
+    });
+};
