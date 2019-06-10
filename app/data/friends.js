4a868f602354787918320506e968ec19da2a9573
Phillip Booker <phillippbooker@gmail.com>
2019-06-09 17:47:10 -0700

    Increases survey to 10 questions. Updates questions.

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,12 +2,12 @@ var friendArray = [
     {
         name: "Jerry",
         pic: "https://picsum.photos/200",
-        scores: [2, 4, 1, 5, 1]
+        scores: [2, 4, 1, 5, 1, 1, 5, 5, 2, 7]
     },
     {
         name: "Joey Wheeler",
         pic: "https://picsum.photos/200",
-        scores: [3, 5, 2, 1, 4]
+        scores: [3, 5, 2, 1, 4, 2, 2, 2, 5, 1]
     }
 ];
 
